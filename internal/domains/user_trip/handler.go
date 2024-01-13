4d6676d85c690d8821de1d0e031b3e7068678201
vynious <shawnthiah@gmail.com>
2024-01-13 15:29:55 +0800

    usertrip handler... previous commit was usertrip service

--- internal/domains/user_trip/handler.go
+++ internal/domains/user_trip/handler.go
@@ -1,1 +1,117 @@
 package user_trip
+
+import (
+	"encoding/json"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"strconv"
+)
+
+type UserTripHandler struct {
+	*UserTripService
+}
+
+func NewUserTripHandler(s *UserTripService) *UserTripHandler {
+	return &UserTripHandler{
+		s,
+	}
+}
+
+func (h *UserTripHandler) AddUserToTrip(w http.ResponseWriter, r *http.Request) {
+	var userReq AddUserTripRequest
+	if err := json.NewDecoder(r.Body).Decode(&userReq); err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	userId := userReq.UserId
+	tripId := userReq.TripId
+
+	ut, err := h.CreateNewUserTrip(r.Context(), userId, tripId)
+	if err != nil {
+		http.Error(w, "failed to create usertrip", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	response := UserTripDetailResponse{
+		UserTrip: ut,
+	}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "failed to write response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *UserTripHandler) GetAllTripsForUserId(w http.ResponseWriter, r *http.Request) {
+
+	id := chi.URLParam(r, "id")
+	uts, err := h.GetUserTripByUserId(r.Context(), id)
+	if err != nil {
+		http.Error(w, "failed to get user trips", http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := UserTripsDetailsResponse{
+		UserTrips: uts,
+	}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "failed to write response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *UserTripHandler) GetAllUsersOnTripId(w http.ResponseWriter, r *http.Request) {
+	strId := chi.URLParam(r, "id")
+	id, err := strconv.ParseInt(strId, 10, 64)
+	if err != nil {
+		http.Error(w, "invalid id params", http.StatusInternalServerError)
+		return
+	}
+
+	uts, err := h.GetUserTripByTripId(r.Context(), id)
+	if err != nil {
+		http.Error(w, "failed to get user trips", http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := UserTripsDetailsResponse{
+		UserTrips: uts,
+	}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "failed to write response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *UserTripHandler) DeleteUserFromTripId(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	uid := query.Get("uid")
+	strTID := query.Get("tid")
+
+	tid, err := strconv.ParseInt(strTID, 10, 64)
+	if err != nil {
+		http.Error(w, "invalid tid params", http.StatusInternalServerError)
+		return
+	}
+
+	ut, err := h.DeleteUserTripById(r.Context(), uid, tid)
+	if err != nil {
+		http.Error(w, "failed to delete user trip", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	response := UserTripDetailResponse{
+		UserTrip: ut,
+	}
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "failed to write response", http.StatusInternalServerError)
+		return
+	}
+}
--- internal/domains/user_trip/user_trip.go
+++ internal/domains/user_trip/user_trip.go
@@ -1,1 +1,16 @@
 package user_trip
+
+import db "github.com/vynious/go-travel/internal/db/sqlc"
+
+type AddUserTripRequest struct {
+	UserId string
+	TripId int64
+}
+
+type UserTripDetailResponse struct {
+	UserTrip db.UserTrip
+}
+
+type UserTripsDetailsResponse struct {
+	UserTrips []db.UserTrip
+}
