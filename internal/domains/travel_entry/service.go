e7da70aced30d204f344c190a5baa0ecaf3700d6
vynious <shawnthiah@gmail.com>
2024-01-17 16:16:10 +0800

    travel entry services

--- internal/domains/travel_entry/service.go
+++ internal/domains/travel_entry/service.go
@@ -36,25 +36,62 @@ func (s *TravelEntryService) CreateNewTravelEntry(ctx context.Context, uid strin
 	return entry, nil
 }
 
-func (s *TravelEntryService) GetTravelEntryById(ctx, eid int64) (db.TravelEntry, error) {
-
+func (s *TravelEntryService) GetTravelEntryById(ctx context.Context, eid int64) (db.TravelEntry, error) {
+	entry, err := s.repository.Queries.GetTravelEntryById(ctx, eid)
+	if err != nil {
+		return db.TravelEntry{}, fmt.Errorf("[service] failed to get travel entry: %w", err)
+	}
+	return entry, nil
 }
 
-func (s *TravelEntryService) GetTravelEntriesByTripId(ctx, tid int64) ([]db.TravelEntry, error) {
-
+func (s *TravelEntryService) GetTravelEntriesByTripId(ctx context.Context, tid int64) ([]db.TravelEntry, error) {
+	entries, err := s.repository.Queries.GetAllTravelEntryByTripId(ctx, tid)
+	if err != nil {
+		return []db.TravelEntry{}, fmt.Errorf("[service] failed to get travel entries: %w", err)
+	}
+	return entries, nil
 }
 
-func (s *TravelEntryService) GetTravelEntriesByUserId(ctx, uid string) ([]db.TravelEntry, error) {
-
+func (s *TravelEntryService) GetTravelEntriesByUserIdAndTripId(ctx context.Context, uid string, tid int64) ([]db.TravelEntry, error) {
+	params := db.GetAllTravelEntryByUserIdAndTripIdParams{
+		TripID: tid,
+		UserID: uid,
+	}
+	entries, err := s.repository.Queries.GetAllTravelEntryByUserIdAndTripId(ctx, params)
+	if err != nil {
+		return []db.TravelEntry{}, fmt.Errorf("[service] failed to get travel entries: %w", err)
+	}
+	return entries, nil
 }
 
-func (s *TravelEntryService) UpdateTravelEntryLocationById(ctx, eid int64, location string) (db.TravelEntry, error) {
-
+func (s *TravelEntryService) UpdateTravelEntryLocationById(ctx context.Context, eid int64, location string) (db.TravelEntry, error) {
+	params := db.UpdateTravelEntryLocationParams{
+		Location: location,
+		ID:       eid,
+	}
+	entry, err := s.repository.Queries.UpdateTravelEntryLocation(ctx, params)
+	if err != nil {
+		return db.TravelEntry{}, fmt.Errorf("[service] failed to update travel entry: %w", err)
+	}
+	return entry, nil
 }
-func (s *TravelEntryService) UpdateTravelEntryDescriptionById(ctx, eid int64, description string) (db.TravelEntry, error) {
-
+func (s *TravelEntryService) UpdateTravelEntryDescriptionById(ctx context.Context, eid int64, description string) (db.TravelEntry, error) {
+	params := db.UpdateTravelEntryDescriptionParams{
+		Description: description,
+		ID:          eid,
+	}
+	entry, err := s.repository.Queries.UpdateTravelEntryDescription(ctx, params)
+	if err != nil {
+		return db.TravelEntry{}, fmt.Errorf("[service] failed to update travel entry: %w", err)
+	}
+	return entry, nil
 }
 
-func (s *TravelEntryService) DeleteTravelEntryById(ctx, eid int64) (db.TravelEntry, error) {
+func (s *TravelEntryService) DeleteTravelEntryById(ctx context.Context, eid int64) (db.TravelEntry, error) {
+	entry, err := s.repository.Queries.DeleteTravelEntry(ctx, eid)
+	if err != nil {
+		return db.TravelEntry{}, fmt.Errorf("[service] failed to delete travel entry: %w", err)
+	}
+	return entry, nil
 
 }
