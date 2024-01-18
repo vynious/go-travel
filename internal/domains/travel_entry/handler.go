f66abbc196e21b9f1be91a14dca858e91bbb308c
vynious <shawnthiah@gmail.com>
2024-01-19 01:34:21 +0800

    travel entry handler template

--- internal/domains/travel_entry/handler.go
+++ internal/domains/travel_entry/handler.go
@@ -1,1 +1,41 @@
 package travel_entry
+
+import "net/http"
+
+type TravelEntryHandler struct {
+	*TravelEntryService
+}
+
+func NewTravelEntryHandler(s *TravelEntryService) *TravelEntryHandler {
+	return &TravelEntryHandler{
+		s,
+	}
+}
+
+func (h *TravelEntryHandler) EnterTravelEntry(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *TravelEntryHandler) ViewTravelEntry(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *TravelEntryHandler) ViewTravelEntriesUnderTrip(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *TravelEntryHandler) ViewAllTravelEntriesUnderTrip(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *TravelEntryHandler) ViewTravelEntriesUnderTripAndUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *TravelEntryHandler) UpdateTravelEntry(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *TravelEntryHandler) DeleteTravelEntry(w http.ResponseWriter, r *http.Request) {
+
+}
