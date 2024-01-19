2335b3cc71a4758d7dbfe8bf7e7f7eaaf0f32478
vynious <shawnthiah@gmail.com>
2024-01-19 13:24:59 +0800

    add travel entry routes

--- internal/http/router.go
+++ internal/http/router.go
@@ -3,13 +3,18 @@ package http
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"github.com/vynious/go-travel/internal/domains/travel_entry"
 	"github.com/vynious/go-travel/internal/domains/trip"
 	"github.com/vynious/go-travel/internal/domains/user"
 	"github.com/vynious/go-travel/internal/domains/user_trip"
 	"net/http"
 )
 
-func InitRouter(userHandler *user.UserHandler, tripHandler *trip.TripHandler, usertripHandler *user_trip.UserTripHandler) chi.Router {
+func InitRouter(
+	userHandler *user.UserHandler,
+	tripHandler *trip.TripHandler,
+	usertripHandler *user_trip.UserTripHandler,
+	travelEntryHandler *travel_entry.TravelEntryHandler) chi.Router {
 	r := chi.NewRouter()
 
 	r.Get("/", func(writer http.ResponseWriter, request *http.Request) {
@@ -18,33 +23,39 @@ func InitRouter(userHandler *user.UserHandler, tripHandler *trip.TripHandler, us
 
 	})
 
-	r.Route("/user", func(r chi.Router) {
-		r.Post("/create", userHandler.RegisterUser)
+	r.Route("/users", func(r chi.Router) {
+		r.Post("/", userHandler.RegisterUser)
 		r.Post("/token", userHandler.GenerateToken)
-		r.Get("/view/{id}", userHandler.ViewUserDetails)
-		r.Get("/view/all", userHandler.ViewAllUsers)
+		r.Get("/{id}", userHandler.ViewUserDetails)
+		r.Get("/", userHandler.ViewAllUsers)
 		r.Get("/search", userHandler.SearchUser)
-		r.Patch("/update/{id}/profile_picture", userHandler.ChangeUserProfilePicture)
-		r.Patch("/update/{id}/details", userHandler.ChangeUserDetails)
-		r.Delete("/delete/{id}", userHandler.DeleteAccount)
-
-		r.Get("/view-trips/{id}", usertripHandler.GetAllTripsForUserId)
+		r.Patch("/{id}/profile-picture", userHandler.ChangeUserProfilePicture)
+		r.Patch("/{id}/details", userHandler.ChangeUserDetails)
+		r.Delete("/{id}", userHandler.DeleteAccount)
 	})
 
-	r.Route("/trip", func(r chi.Router) {
-		r.Post("/create", tripHandler.StartTrip)
-		r.Get("/view/{id}", tripHandler.ViewTripDetails)
-		r.Get("/view/all", tripHandler.ViewAllTrips)
-		r.Patch("/update/{id}", tripHandler.ChangeTripDetails)
-		r.Delete("/delete/{id}", tripHandler.DeleteTrip)
-
-		r.Get("/view-users/{id}", usertripHandler.GetAllUsersOnTripId)
-
+	r.Route("/trips", func(r chi.Router) {
+		r.Post("/", tripHandler.StartTrip)
+		r.Get("/{id}", tripHandler.ViewTripDetails)
+		r.Get("/", tripHandler.ViewAllTrips)
+		r.Patch("/{id}", tripHandler.ChangeTripDetails)
+		r.Delete("/{id}", tripHandler.DeleteTrip)
 	})
 
-	r.Route("/assign-trip", func(r chi.Router) {
-		r.Post("/add-user", usertripHandler.AddUserToTrip)
-		r.Post("/remove-user", usertripHandler.DeleteUserFromTripId)
+	r.Route("/trip-assignments", func(r chi.Router) {
+		r.Post("/{tripId}/users", usertripHandler.AddUserToTrip)
+		r.Delete("/{tripId}/users/{userId}", usertripHandler.DeleteUserFromTripId)
+		r.Get("/{tripId}/users", usertripHandler.GetAllUsersOnTripId)
+		r.Get("/users/{userId}", usertripHandler.GetAllTripsForUserId)
+	})
+
+	r.Route("/travel-entries", func(r chi.Router) {
+		r.Post("/", travelEntryHandler.EnterTravelEntry)
+		r.Get("/{id}", travelEntryHandler.ViewTravelEntry)
+		r.Get("/trips/{tripId}", travelEntryHandler.ViewTravelEntriesUnderTrip)
+		r.Get("/users/{userId}/trips/{tripId}", travelEntryHandler.ViewTravelEntriesUnderTripAndUser)
+		r.Patch("/{id}", travelEntryHandler.UpdateTravelEntry)
+		r.Delete("/{id}", travelEntryHandler.DeleteTravelEntry)
 	})
 
 	return r
