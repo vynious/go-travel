75014a969a2aaa7c3c2d00d4919f3d2d23848bcc
vynious <shawnthiah@gmail.com>
2024-01-26 23:44:45 +0800

    connection handler template

--- internal/domains/connections/handler.go
+++ internal/domains/connections/handler.go
@@ -1,1 +1,25 @@
 package connections
+
+import "net/http"
+
+type ConnectionHandler struct {
+	*ConnectionService
+}
+
+func NewConnectionHandler(s *ConnectionService) *ConnectionHandler {
+	return &ConnectionHandler{
+		s,
+	}
+}
+
+func (h *ConnectionHandler) MakeConnection(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *ConnectionHandler) ViewConnection(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *ConnectionHandler) RemoveConnection(w http.ResponseWriter, r *http.Request) {
+
+}
